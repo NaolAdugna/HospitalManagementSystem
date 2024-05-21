a4c4aa0ad926e498e376d36426e16f0ddb096ddb
Naol Adugna <115176230+NaolAdugna@users.noreply.github.com>
2024-05-21 11:21:03 +0300

    backend updated.

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,8 @@ import ENV from "../../config.js";
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
-const urlDB = `mysql://root:hfDGinZxnzGoogBcedPgtcGUzGPADVzS@viaduct.proxy.rlwy.net:34677/railway`;
+// const urlDB = `mysql://root:hfDGinZxnzGoogBcedPgtcGUzGPADVzS@viaduct.proxy.rlwy.net:34677/railway`;
+const urlDB = `jdbc:mysql://sql12.freesqldatabase.com:3306/sql12708030`;
 
 const mysqlPool = mysql.createPool(urlDB);
 
