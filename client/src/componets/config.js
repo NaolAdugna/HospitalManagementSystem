39e6f6ed86105635fed8c7503e2461621247d371
Naol Adugna <115176230+NaolAdugna@users.noreply.github.com>
2024-05-27 22:13:29 +0300

    gitignore fixed.

---
+++ client/src/componets/config.js
@@ -0,0 +1,5 @@
+const config = {
+  Private_Key: "cddc2e32-98ec-45ae-8b4a-691f47f4d0ba", // Project id of chat engine io value
+  API_KEY: "AIzaSyAOCLhOVfnEUQD1RjarL0xHljhoNOh6zrU",
+};
+export default config;
